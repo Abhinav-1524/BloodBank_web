b12481416bdaf2f6b3198772fe59d6998e4be6e8
Aaryanajith <aaryanajith45@gmail.com>
2023-06-08 18:28:21 +0530

    changes to recieve and cards page

--- frontend/src/pages/card.jsx
+++ frontend/src/pages/card.jsx
@@ -1,27 +1,38 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Card from '@mui/material/Card';
 import CardContent from '@mui/material/CardContent';
-import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
+import './cards.css'
 
-const CustomCard = ({ title, bloodGroup ,locality,contact}) => {
+const CustomCard = ({ title, bloodGroup, locality, contact }) => {
+  const [showSnackbar, setShowSnackbar] = useState(false);
 
+  const handleCopyClick = () => {
+    navigator.clipboard.writeText(contact);
+    setShowSnackbar(true);
+    setTimeout(() => setShowSnackbar(false), 2000);
+  };
 
-return (
+  return (
     <Card sx={{ maxWidth: 345, boxShadow: '0px 0px 10px rgba(0, 0, 0, 0.2)', borderRadius: '12px' }}>
-      
-    <CardContent sx={{ paddingBottom: '16px !important' }}>
-    <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: 'bold', marginBottom: '8px' }}>
-      {title}
-    </Typography>
-      <Typography variant="body2" color="text.secondary" sx={{ marginBottom: '8px' }}>
-      <span style={{ fontWeight: 'bold' }}>Blood Group:</span> {bloodGroup} <br />
-      <span style={{ fontWeight: 'bold' }}>Locality:</span> {locality} <br />
-      <span style={{ fontWeight: 'bold' }}>Contact:</span> {contact}
-      </Typography>
-    </CardContent>
+      <CardContent sx={{ paddingBottom: '16px !important' }}>
+        <Typography gutterBottom variant="h5" component="div" sx={{ fontWeight: 'bold', marginBottom: '8px' }}>
+          {title}
+        </Typography>
+        <Typography variant="body2" color="text.secondary" sx={{ marginBottom: '8px' }}>
+          <span style={{ fontWeight: 'bold' }}>Blood Group:</span> {bloodGroup} <br />
+          <span style={{ fontWeight: 'bold' }}>Locality:</span> {locality} <br />
+          <span style={{ fontWeight: 'bold' }}>Contact:</span> {contact}
+          <button onClick={handleCopyClick} style={{ marginLeft: '8px' }}>
+            Copy
+          </button>
+        </Typography>
+      </CardContent>
+      {showSnackbar && (
+        <div className="snackbar">Text copied!</div>
+      )}
     </Card>
-);
+  );
 };
 
 export default CustomCard;
